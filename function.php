c2b4c78772f00e6412fb8ccbe4133400a8f541c1
antoninoweb <37928695+antoninoweb@users.noreply.github.com>
2018-04-04 14:37:49 +0200

    Update function.php

--- function.php
+++ function.php
@@ -5,12 +5,11 @@ function rotateImage($a) {
     $myarray = array_reverse($a);
 
             foreach($myarray as $key => $value){
-        $k = 0;
+      
                 foreach($value as $key2 => $value2){
                     
                     $finish[$key2][$key] = $value2;
-                   
-                    $k++; 
+                  
                 }
             }
     
